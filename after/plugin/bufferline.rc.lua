98331ce516692fa00eaaf9367c21dc407debf720
mdrokz <mohammadmunshi@gmail.com>
2022-10-31 14:40:16 +0530

    configure bufferline & lualine to use nord colorscheme

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -1,35 +1,21 @@
 local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
+local highlights = pcall(require,"nord").bufferline.highlights({
+  italic = true,
+  bold = true
+})
+
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
+    separator_style = 'thin',
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
     color_icons = true
   },
-  highlights = {
-    separator = {
-      fg = '#073642',
-      bg = '#002b36',
-    },
-    separator_selected = {
-      fg = '#073642',
-    },
-    background = {
-      fg = '#657b83',
-      bg = '#002b36'
-    },
-    buffer_selected = {
-      fg = '#fdf6e3',
-      bold = true,
-    },
-    fill = {
-      bg = '#073642'
-    }
-  },
+  highlights = highlights
 })
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
